875f400209128b607d18450f5fe9c483bcd65329
shashwotbhattarai <shashwot.tech@gmail.com>
2025-07-26 20:46:51 +0545

    formated the problems

--- typescript/src/array&hashing/containsDuplicate.ts
+++ typescript/src/array&hashing/containsDuplicate.ts
@@ -1,5 +1,53 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ 
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
 function containsDuplicate(nums: number[]): boolean {
 	return nums.length !== new Set(nums).size;
 }
 
 console.log(containsDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(containsDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9]));
+
+/*
+when tryig to convert nums into a set
+time complexity == O(n)
+
+space of a set
+space complexity == O(n)
+*/
--- typescript/src/array&hashing/validAnagram.ts
+++ typescript/src/array&hashing/validAnagram.ts
@@ -1,3 +1,29 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+*/
+
 function validAnagram(s: string, t: string): boolean {
 	if (s.length !== t.length) {
 		return false;
@@ -21,3 +47,16 @@ function validAnagram(s: string, t: string): boolean {
 }
 
 console.log(validAnagram("abc", "abcd"));
+console.log(validAnagram("bcdggh", "bcdggi"));
+console.log(validAnagram("rat", "tar"));
+
+/*
+
+time complexity == O(n+m)
+n being length of string, since both strings must be of equal size we can consider there length n
+m is the lenght of frequency hash map of either strings. since both the strings are of equal size, 
+their hashmap size will also be equal and can be cosidered m.
+
+space complexity == O(c+2n)
+
+*/
